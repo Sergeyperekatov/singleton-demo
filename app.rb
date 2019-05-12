e8a8589dd754a206ea043399ce281e54d791bdaa
Sergey Perekatov <perekatovsergey@yandex.ru>
2019-05-12 17:19:30 +0300

    update app.rb

--- app.rb
+++ app.rb
@@ -1,23 +1,6 @@
-class Logger
-  def initialize
-    @f = File.open 'log.txt', 'a'
-  end
+require "./logger"
 
-@@x = Logger.new
-  
-  def self.instance
-     return @@x
-  end
-  
-    # instance method
-  def log_foo bar
-   @f.puts bar
-  end
-
-  private_class_method :new
-
-end
-
+Loggeer.instance.log "It`s work!"
 
 
 
